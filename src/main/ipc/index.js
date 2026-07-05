80eeb5497058ee7ce05f6cb9275cba32f704df57
DavidAr55 <davidarturoloera@gmail.com>
2026-07-05 16:33:48 -0600

    chore: register app, multimedia, backup, fonts, watermark and search IPC handlers

--- src/main/ipc/index.js
+++ src/main/ipc/index.js
@@ -1,4 +1,5 @@
 import { createRepositories }       from '../db/repositories/index.js'
+import { registerAppIPC }           from './app.ipc.js'
 import { registerLibraryIPC }       from './library.ipc.js'
 import { registerSettingsIPC }      from './settings.ipc.js'
 import { registerProjectionIPC }    from './projection.ipc.js'
@@ -7,15 +8,26 @@ import { registerBibleIPC }         from './bible.ipc.js'
 import { registerSongsIPC }         from './songs.ipc.js'
 import { registerPresentationsIPC } from './presentations.ipc.js'
 import { registerBackgroundsIPC }   from './backgrounds.ipc.js'
+import { registerMultimediaIPC }    from './multimedia.ipc.js'
+import { registerBackupIPC }        from './backup.ipc.js'
+import { registerFontsIPC }         from './fonts.ipc.js'
+import { registerWatermarkIPC }      from './watermark.ipc.js'
+import { registerSearchIPC }         from './search.ipc.js'
 
 export function registerAllIPC(db, windowManager, bgServerPort = 0) {
   const repos = createRepositories(db)
+  registerAppIPC()
   registerLibraryIPC(repos.library)
   registerSettingsIPC(repos.settings)
   registerProjectionIPC(windowManager)
-  registerDisplaysIPC()
+  registerDisplaysIPC(windowManager)
   registerBibleIPC(repos.bible)
-  registerSongsIPC(repos.songs)
+  registerSongsIPC(repos.songs, windowManager)
   registerPresentationsIPC(repos.presentations, windowManager)
   registerBackgroundsIPC(repos.backgrounds, windowManager, bgServerPort)
+  registerMultimediaIPC(repos.media, windowManager, bgServerPort)
+  registerBackupIPC(db, windowManager)
+  registerFontsIPC()
+  registerWatermarkIPC(windowManager)
+  registerSearchIPC(repos) // buscador global: agrega varios dominios, recibe todos los repos
 }
